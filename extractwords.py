22b6aad21d835f06f4fd68c196c07a406275dc85
Morgan Ericsson <morganericsson@gmail.com>
2013-09-10 13:30:15 +0200

    fixed word extraction

--- extractwords.py
+++ extractwords.py
@@ -1,7 +1,8 @@
 import sys
 from collections import Counter
+import string
 
 mytext = open(sys.argv[1]).read()
-freq = Counter(mytext.split())
+freq = Counter([ w.translate(None, string.punctuation).lower() for w in mytext.split() ])
 
 print freq
